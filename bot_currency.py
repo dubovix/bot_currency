03c813ec14021acd5563ba5de3708d99994ecb01
dubovix <a.a.dubovik@gmail.com>
2024-07-25 18:55:41 +0300

    Added euro currency

--- bot_currency.py
+++ bot_currency.py
@@ -13,9 +13,13 @@ def update_message(message):
     response = urlopen("https://kurs.onliner.by/")
     soup = BeautifulSoup(response)
     tags = soup.find_all('p', {'class':"value"})
+    print(tags)
     buy = tags[0].text
     sell = tags[1].text
+    euro_buy = tags[3].text
+    euro_sell = tags[4].text
 
-    bot.send_message(message.chat.id, "buy - " + buy + ", sell - " + sell)
+    bot.send_message(message.chat.id, "buy $ - " + buy + ", sell $ - " + sell)
+    bot.send_message(message.chat.id, "buy € - " + euro_buy + ", sell € - " + euro_sell)
 
 bot.polling()
